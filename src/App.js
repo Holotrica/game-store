de6f8f4c0aa89d9966463d27e3feaa3b2e867e22
perdanamain <firman.fp123@gmail.com>
2025-02-07 10:54:38 +0700

    fix: change facebook auth version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from "./logo.svg";
-import styles from "./App.module.css";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  useNavigate,
-  useLocation,
-} from "react-router-dom";
+/* eslint-disable eqeqeq */
+import { AnimatePresence } from "framer-motion";
 import React, { useEffect, useState } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import Browse from "./Containers/Browse/Browse";
 import GamePage from "./Containers/GamePage/GamePage";
-import NotFound from "./Containers/NotFound/NotFound";
 import Home from "./Containers/Home/Home";
-import { AnimatePresence } from "framer-motion";
+import NotFound from "./Containers/NotFound/NotFound";
+import { AuthProvider } from "./utils/AuthContext";
 import filterNames from "./utils/filterNames";
 import games from "./utils/games";
 import templateGame from "./utils/templateGame";
-import { AuthProvider } from './utils/AuthContext';
-import Login from './Components/Login';
 
 function AppContent() {
   const [currentFilter, setCurrentFilter] = useState("none");
